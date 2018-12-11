1e0dadda615d22a66daa24438104f8e3b187a269
Gerard Del Castillo <gerardj.delcastillo@gmail.com>
2018-12-10 21:29:56 -0800

    Did This Work?

---
+++ src/fraction.cc
@@ -0,0 +1,54 @@
+#include "fractions.h"
+#include <iostream>
+
+namespace edu
+{
+    namespace vcccd
+    {
+        namespace vc
+        {
+            namespace csv13
+            {
+                int32_t Fraction::getNumerator() const {
+                    return numerator;
+                }
+
+                int32_t Fraction::getDenominator() const {
+                    return denominator;
+                }
+
+                void Fraction::setNumerator(int32_t &numerator) {
+                    this->numerator = numerator;
+                }
+
+                void Fraction::setDenominator(int32_t denominator) {
+                    this->denominator = denominator;
+                }
+
+                int gcd(int numerator, int denominator) {
+                    while (denominator != 0){
+                        int temp = numerator%denominator;
+                        numerator = denominator;
+                        denominator = temp;
+                    } return numerator;
+                }
+
+                void Fraction::output() {
+                    if (numerator == 0 && denominator == 0) {
+                        std::cout << "NAN";
+                    } else if (numerator == 0) {
+                        std::cout << numerator;
+                    } else if (denominator == 0) {
+                        std::cout << "INF";
+                    } else if (numerator % denominator == 0) {
+                        std::cout << numerator / denominator;
+                    } else if (numerator % denominator != 0) {
+                        int numNum = numerator / gcd(numerator, denominator);
+                        int denNum = denominator / gcd(numerator, denominator);
+                        std::cout << numNum << " / " << denNum;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main.cc
@@ -0,0 +1,14 @@
+//
+// Created by Gerard Del Castillo on 12/4/2018.
+//
+
+#include <iostream>
+#include "fractions.h"
+
+using edu::vcccd::vc::csv13::Fraction;
+
+int main()
+{
+    Fraction f(9, 17);
+    f.output();
+}
